1c8859b12eba2179c1b8222057142b7a07c46ed1
Strikeskids <github@strikeskids.com>
2013-08-02 10:49:28 -0400

    Fixed isReady() problem

--- src/com/sk/methods/action/Action.java
+++ src/com/sk/methods/action/Action.java
@@ -1,5 +1,8 @@
 package com.sk.methods.action;
 
+import org.powerbot.script.wrappers.Component;
+import org.powerbot.script.wrappers.Widget;
+
 import com.sk.SkMethodContext;
 import com.sk.methods.action.structure.Ability;
 import com.sk.methods.action.structure.BarIcon;
@@ -10,6 +13,11 @@ public class Action extends org.powerbot.script.wrappers.Action {
 	protected SkMethodContext ctx;
 	private BarIcon icon;
 	private final int slot;
+
+	private static final int WIDGET_ID = 1430;
+	private static final int COOLDOWN_CHILD = 97;
+	private static final int AVAILABILITY_CHILD = 96;
+	private static final int AVAILABLE_TEXT_COLOR = 0xFFFFFF;
 
 	public Action(SkMethodContext ctx, int slot, Type type, int id, BarIcon ico) {
 		super(ctx, slot, type, id);
@@ -37,5 +45,29 @@ public class Action extends org.powerbot.script.wrappers.Action {
 	@Override
 	public boolean select() {
 		return isValid() && ctx.keyboard.key(getBind(), 0);
+	}
+
+	@Override
+	public boolean isReady() {
+		if (!isValid())
+			return false;
+		return getAvailabilityComponent().getTextColor() == AVAILABLE_TEXT_COLOR
+				&& !getCooldownComponent().isVisible();
+	}
+
+	public Component getAvailabilityComponent() {
+		if (0 <= slot && slot < 12)
+			return getWidget().getComponent(AVAILABILITY_CHILD + 4 * slot);
+		return ctx.widgets.get(0, -1);
+	}
+
+	public Component getCooldownComponent() {
+		if (0 <= slot && slot < 12)
+			return getWidget().getComponent(COOLDOWN_CHILD + 4 * slot);
+		return ctx.widgets.get(0, -1);
+	}
+
+	private Widget getWidget() {
+		return ctx.widgets.get(WIDGET_ID);
 	}
 }
